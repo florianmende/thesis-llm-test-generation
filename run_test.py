d4bd036d50feda228359ebc4231ce76e522727e9
florianmende <57731341+florianmende@users.noreply.github.com>
2023-09-27 12:40:21 +0200

    Modularization of project and test compilation and test execution

--- run_test.py
+++ run_test.py
@@ -2,98 +2,156 @@ import glob
 import os
 import subprocess
 import configparser
+import xml.etree.ElementTree as ET
 
 
 class TestExecuter:
 
-    def __init__(self):
+    def __init__(self, project_name: str):
+        self.project_name = project_name
         self.dependencies = []
 
-    def compile_java_project(self, project_path, output_path):
-        """
-        Compile a java project using maven
-        :param project_name:
-        :return:
-        """
+        self.current_abs_path = os.getcwd()
 
-    def complile_test_case(self, dependencies, compiled_test_dir):
-        current_dir = os.getcwd()
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        mockito_jar = config.get('JARS', 'MOCKITO_JAR')
-        junit_jar = config.get('JARS', 'JUNIT_JAR')
-        classpath = f"{dependencies}:{mockito_jar}:{junit_jar}"
-        classpath_file = os.path.join(compiled_test_dir, 'classpath.txt')
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        self.MOCKITO_JAR = self.config.get('JARS', 'MOCKITO_JAR')
+        self.JUNIT_JAR = self.config.get('JARS', 'JUNIT_JAR')
+
+    def get_dependencies_as_string(self):
+        return ":".join(self.dependencies)
+
+    def compile_test_case(self, classpath_file_name, test_file_path):
+        """
+        Compile a test case using javac
+        :param classpath_file_name: Name for the classpath file
+        :param test_file_path: Path to the test file to compile (relative to build/[project_name] dir)
+        :return: Tuple of return code and output of the javac command (0 if successful, 1 if not)
+        """
+        classpath_file_path = f"{self.current_abs_path}/build/artifacts/classpaths_tests_compilation/{self.project_name}"
+        if not os.path.exists(classpath_file_path):
+            os.makedirs(classpath_file_path)
+        classpath = f"{self.get_dependencies_as_string()}:{self.MOCKITO_JAR}:{self.JUNIT_JAR}"
+        classpath_file = os.path.join(classpath_file_path, classpath_file_name)
         self.export_classpath(classpath_file, classpath)
+
+        # Compile the test case
         result = subprocess.run(
-            f"javac -cp {current_dir}/Java_Projects/jfreechart/target -d {current_dir}/build/compiled_tests @{classpath_file} {current_dir}/Java_Projects/jfreechart/src/generated_tests/java/org/jfree/tests/10_test.java",
+            f"javac -cp {self.current_abs_path}/build/compiled_projects/{self.project_name} \
+            -d {self.current_abs_path}/build/compiled_tests/{self.project_name} \
+            @{classpath_file} \
+            {self.current_abs_path}/build/generated_tests/{self.project_name}/{test_file_path}",
             shell=True, capture_output=True, text=True)
         output = result.stdout if result.returncode == 0 else result.stderr
         print(output)
         return result.returncode, output
 
     @staticmethod
-    def export_classpath(classpath_file, classpath):
+    def export_classpath(classpath_file: str, classpath: str):
+        """
+        Write a list of dependencies seperated by : (classpath) to a file for later use
+        :param classpath_file: filepath to write the classpath to
+        :param classpath: classpath to write to the file (list of dependencies seperated by :)
+        :return:
+        """
         with open(classpath_file, 'w') as f:
             classpath = "-cp " + classpath
             f.write(classpath)
         return
 
-    def get_dependencies(self, project_name):
+    def get_standard_compile_path(self):
+        pom_file = f"{self.current_abs_path}/Java_Projects/{self.project_name}/pom.xml"
+
+        # read pom.xml and check if there is a OutputDirectory tag
+        # if there is, use that as the standard compile path
+        # if there is not, use the default path
+        # Parse the XML file
+        tree = ET.parse(pom_file)
+        root = tree.getroot()
+
+        # Namespace for Maven POM
+        ns = {'maven': 'http://maven.apache.org/POM/4.0.0'}
+
+        # Extract the outputDirectory
+        output_directory_element = root.find('./maven:build/maven:outputDirectory', ns)
+        if output_directory_element is not None:
+            return output_directory_element.text
+        else:
+            return None
+
+    def make_dependencies(self):
         """
-        Get runtime dependencies of a test using maven
+        Generate dependencies for a java project using maven and save them in the dependencies list
+        """
+        mvn_target_dir = f'{self.current_abs_path}/build/compiled_projects/{self.project_name}'
+        # Check if dependencies are already generated for the specific project
+        if not self.dependencies:
+            print("Making dependencies for project:", self.project_name)
+            # Run mvn command to generate dependencies
+            # Project needs to be a maven project and have a pom.xml file
+            subprocess.run(
+                f"mvn dependency:copy-dependencies -DoutputDirectory={mvn_target_dir}/dependencies \
+                -f {self.current_abs_path}/Java_Projects/{self.project_name}/pom.xml",
+                shell=True)
+
+            # Install the project using maven (also compiles the project)
+            # Tests are skipped to avoid running old tests from the project
+            subprocess.run(f"mvn install -DskipTests -f {self.current_abs_path}/Java_Projects/{self.project_name}/pom.xml",
+                           shell=True)
+
+            # copy compiled files to compiled_projects folder
+            # if no outputDirectory is specified in the pom.xml, the standard directory will be used
+            compile_path = self.get_standard_compile_path() if self.get_standard_compile_path() else "target"
+            target_folder = f"{self.current_abs_path}/build/compiled_projects/{self.project_name}"
+            subprocess.run(f"cp -r {self.current_abs_path}/Java_Projects/{self.project_name}/{compile_path}/* \
+                            {target_folder}", shell=True)
+            # remove files
+            subprocess.run(f"rm -r {self.current_abs_path}/Java_Projects/{self.project_name}/{compile_path}", shell=True)
+
+            # Get paths of all dependencies (jars) and add them to the dependencies list
+            dep_jars = glob.glob(f"{mvn_target_dir}" + "/**/*.jar", recursive=True)
+            self.dependencies.extend(list(set(dep_jars)))
+            print("Dependencies generated for project:", self.project_name)
+        else:
+            print("Dependencies already generated for project:", self.project_name)
+
+    def run_test(self, classpath_file_name, class_to_test):
+        """
+        Run a test using java and junit
+        :param classpath_file_name: File name where the classpath should be saved (has to be a txt)
+        :param class_to_test: Class which should be run as a test (e.g. org.jfree.tests.junit.chart.JFreeChartTests)
         :return:
         """
-        mvn_dependency_dir = 'target/dependency'
-        deps = []
-        # Run mvn command to generate dependencies
-        # print("Making dependency for project", self.target_path)
-        current_dir = os.getcwd()
-        subprocess.run(
-            f"mvn dependency:copy-dependencies -DoutputDirectory={mvn_dependency_dir} -f {current_dir}/Java_Projects/{project_name}/pom.xml",
-            shell=True)
-        subprocess.run(f"mvn install -DskipTests -f {current_dir}/Java_Projects/{project_name}/pom.xml", shell=True)
 
-        dep_jars = glob.glob(f"{current_dir}/Java_Projects/{project_name}" + "/**/*.jar", recursive=True)
-        deps.extend(dep_jars)
-        deps = list(set(deps))
-        return ':'.join(deps)
+        classpath = f"{self.JUNIT_JAR}:{self.MOCKITO_JAR}:{self.get_dependencies_as_string()}:" \
+                    f"{self.current_abs_path}/build/compiled_projects/{self.project_name}/classes:" \
+                    f"{self.current_abs_path}/build/compiled_tests/{self.project_name}"
+        classpath_file_path = f"{self.current_abs_path}/build/artifacts/classpaths_tests_execution/{self.project_name}"
 
-    def run_test(self, test_case, dependencies, compiled_test_dir, build_dir):
-        """
-        Run a test using java
-        :param test_case:
-        :param dependencies:
-        :return:
-        """
-        current_dir = os.getcwd()
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        mockito_jar = config.get('JARS', 'MOCKITO_JAR')
-        junit_jar = config.get('JARS', 'JUNIT_JAR')
-        classpath = f"{junit_jar}:{mockito_jar}:{dependencies}:{current_dir}/{build_dir}"
-        classpath_file = os.path.join(compiled_test_dir, 'classpath.txt')
+        if not os.path.exists(classpath_file_path):
+            os.makedirs(classpath_file_path)
+
+        classpath_file = os.path.join(classpath_file_path, classpath_file_name)
         self.export_classpath(classpath_file, classpath)
 
         cmd = ["java",
                 f"@{classpath_file}",
                 "org.junit.platform.console.ConsoleLauncher", "--disable-banner", "--disable-ansi-colors",
                 "--fail-if-no-tests", "--details=none", "--select-class",
-                f"org.jfree.tests.MinuteTest"]
+                f"{class_to_test}"]
 
         result = subprocess.run(" ".join(cmd), shell=True)
-        print(result)
         output = result.stdout if result.returncode == 0 else result.stderr
-        print(output)
         return result.returncode, output
 
 
-test_executer = TestExecuter()
-deps = test_executer.get_dependencies("jfreechart")
-# print(deps)
-print("Compilation step:")
-print(test_executer.complile_test_case(deps, "./build/compiled_tests"))
+test_executer = TestExecuter("jfreechart")
+test_executer.make_dependencies()
 
-javac = test_executer.run_test("10_test.java", deps, "build/compiled_tests/org/jfree/tests/", "build/compiled_tests")
-print("Execution step:")
-print(javac)
+print("Compiling Test Case:")
+print(test_executer.compile_test_case("test_classpath.txt", "/java/org/jfree/tests/10_test.java"))
+
+
+# javac = test_executer.run_test("10_test.java", deps, "build/compiled_tests/org/jfree/tests/", "build/compiled_tests")
+print("Running test case:")
+print(test_executer.run_test("test_classpath.txt", "org.jfree.tests.MinuteTest"))
